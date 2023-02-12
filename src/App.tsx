ea48fc262568d54f9ca33256164755823e5be547
Evan Gray <battledingo@gmail.com>
2023-02-11 21:45:32 -0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,64 @@
 import { GitHub } from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import {
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+  useRouteMatch,
+} from "react-router-dom";
 import Decoder from "./Decoder";
+
+function NavButton({
+  label,
+  to,
+  exact = false,
+}: {
+  label: string;
+  to: string;
+  exact?: boolean;
+}) {
+  let match = useRouteMatch({
+    path: to,
+    exact,
+  });
+  return (
+    <Button
+      component={NavLink}
+      to={to}
+      size="small"
+      disabled={!!match}
+      sx={{
+        pb: 0,
+        px: 0,
+        textUnderlineOffset: 4,
+        textDecoration: match ? "underline" : undefined,
+        "&.Mui-disabled": { color: "text.primary" },
+      }}
+      color="inherit"
+    >
+      {label}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <main>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            VAA Parser
+          <Typography variant="h6" component="div" sx={{ mr: 1 }}>
+            VAA Tools
           </Typography>
+          <NavButton to="/" label="Parse" exact />
+          <Box sx={{ flexGrow: 1 }} />
           <IconButton
             href="https://github.com/evan-gray/vaa-dev"
             edge="end"
@@ -21,7 +70,14 @@ function App() {
         </Toolbar>
       </AppBar>
       <Box m={2}>
-        <Decoder />
+        <Switch>
+          <Route exact path="/">
+            <Decoder />
+          </Route>
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
       </Box>
     </main>
   );
