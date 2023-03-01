153faf16c192f7f74f227638dad48bc1a345bec9
Evan Gray <battledingo@gmail.com>
2023-02-28 22:50:05 -0500

    support gh pages routing

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {
 import { grey } from "@mui/material/colors";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
