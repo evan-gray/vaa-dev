4f0ba27a21651939bca8eb3299afde185d2d50bb
Evan Gray <battledingo@gmail.com>
2023-02-28 22:36:29 -0500

    first pass at highlighting; fixes #5

--- src/Decoder.tsx
+++ src/Decoder.tsx
@@ -1,13 +1,16 @@
 import {
+  Box,
   Card,
   CardContent,
   CardHeader,
   Grid,
+  SxProps,
   TextField,
+  Theme,
   Typography,
 } from "@mui/material";
 import { Buffer } from "buffer";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import {
   parseTokenTransferPayload,
@@ -16,12 +19,17 @@ import {
 } from "./sdk/tokenBridge";
 import { ParsedVaa, parseVaa } from "./sdk/wormhole";
 import chainIdToString from "./utils/chainIdToString";
-import { VaaIndexes, vaaToIndexes } from "./utils/vaaToIndexes";
+import {
+  tokenTransferPayloadToIndexes,
+  VaaIndexes,
+  vaaToIndexes,
+} from "./utils/vaaToIndexes";
 
 type ParsedVaaAndPayload = {
   vaa?: ParsedVaa;
   vaaIndexes?: VaaIndexes;
   tokenBridge?: TokenTransfer;
+  tokenBridgeIndexes?: VaaIndexes;
 };
 
 function idToStr(id: number): string {
@@ -43,36 +51,77 @@ function transferTypeToString(
   return transferType;
 }
 
-const vaaToString = (vaa: ParsedVaa) => `{
-  version: ${vaa.version},
-  guardianSetIndex: ${vaa.guardianSetIndex},
-  guardianSignatures: (${vaa.guardianSignatures.length}),
-  timestamp: ${vaa.timestamp} (${new Date(
-  vaa.timestamp * 1000
-).toLocaleString()}),
-  nonce: ${vaa.nonce},
-  emitterChain: ${idToStr(vaa.emitterChain)},
-  emitterAddress: ${vaa.emitterAddress.toString("hex")},
-  sequence: ${vaa.sequence.toString()},
-  consistencyLevel: ${vaa.consistencyLevel},
-}`;
+const vaaToString = (vaa: ParsedVaa, handleHover: (e: any) => void) =>
+  [
+    `  version: ${vaa.version},`,
+    `  guardianSetIndex: ${vaa.guardianSetIndex},`,
+    `  guardianSignatures: (${vaa.guardianSignatures.length}),`,
+    `  timestamp: ${vaa.timestamp} (${new Date(
+      vaa.timestamp * 1000
+    ).toLocaleString()}),`,
+    `  nonce: ${vaa.nonce},`,
+    `  emitterChain: ${idToStr(vaa.emitterChain)},`,
+    `  emitterAddress: ${vaa.emitterAddress.toString("hex")},`,
+    `  sequence: ${vaa.sequence.toString()},`,
+    `  consistencyLevel: ${vaa.consistencyLevel},`,
+  ].map((s) => {
+    const key = s.split(":")[0].trim();
+    return (
+      <pre
+        key={key}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+        data-index={key}
+      >
+        {s}
+      </pre>
+    );
+  });
 
-const tokenTransferToString = (tokenTransfer: TokenTransfer) => `{
-  payloadType: ${transferTypeToString(tokenTransfer.payloadType)},
-  amount: ${tokenTransfer.amount.toString()},
-  tokenChain: ${idToStr(tokenTransfer.tokenChain)},
-  tokenAddress: ${tokenTransfer.tokenAddress.toString("hex")},
-  toChain: ${idToStr(tokenTransfer.toChain)},
-  toAddress: ${tokenTransfer.to.toString("hex")},
-  ${
-    tokenTransfer.payloadType === TokenBridgePayload.Transfer
-      ? `fee: ${tokenTransfer.fee?.toString()},`
+const tokenTransferToString = (
+  tokenTransfer: TokenTransfer,
+  handleHover: (e: any) => void
+) =>
+  [
+    `  payloadType: ${transferTypeToString(tokenTransfer.payloadType)},`,
+    `  amount: ${tokenTransfer.amount.toString()},`,
+    `  tokenChain: ${idToStr(tokenTransfer.tokenChain)},`,
+    `  tokenAddress: ${tokenTransfer.tokenAddress.toString("hex")},`,
+    `  toChain: ${idToStr(tokenTransfer.toChain)},`,
+    `  toAddress: ${tokenTransfer.to.toString("hex")},`,
+    ...(tokenTransfer.payloadType === TokenBridgePayload.Transfer
+      ? [`  fee: ${tokenTransfer.fee?.toString()},`]
       : tokenTransfer.payloadType === TokenBridgePayload.TransferWithPayload
-      ? `fromAddress: ${tokenTransfer.fromAddress?.toString("hex")},
-  tokenTransferPayload: ${tokenTransfer.tokenTransferPayload.toString("hex")}`
-      : ""
-  }
-}`;
+      ? [
+          `  fromAddress: ${tokenTransfer.fromAddress?.toString("hex")},`,
+          `  tokenTransferPayload: ${tokenTransfer.tokenTransferPayload.toString(
+            "hex"
+          )}`,
+        ]
+      : []),
+  ].map((s) => {
+    const key = s.split(":")[0].trim();
+    return (
+      <pre
+        key={key}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+        data-index={`payload-${key}`}
+      >
+        {s}
+      </pre>
+    );
+  });
+
+const highlightColor = "rgba(255,255,0,0.2)";
+
+const preBoxStyle: SxProps<Theme> = {
+  overflowX: "auto",
+  "& pre": {
+    m: 0,
+    "&:hover": { background: highlightColor },
+  },
+};
 
 export function DecoderComponent({
   vaaString,
@@ -81,6 +130,7 @@ export function DecoderComponent({
   vaaString: string;
   handleHexChange?: (e: any) => void;
 }) {
+  const inputRef = useRef<any>();
   const [parsed, setParsed] = useState<ParsedVaaAndPayload | null>(null);
   useEffect(() => {
     setParsed(null);
@@ -95,16 +145,45 @@ export function DecoderComponent({
       const vaa = parseVaa(buf);
       const vaaIndexes = vaaToIndexes(buf);
       let tokenBridge: TokenTransfer | undefined;
+      let tokenBridgeIndexes: VaaIndexes | undefined;
       try {
         tokenBridge = parseTokenTransferPayload(vaa.payload);
+        tokenBridgeIndexes = tokenTransferPayloadToIndexes(vaa.payload);
       } catch (e) {
         console.error(e);
       }
-      setParsed({ vaa, vaaIndexes, tokenBridge });
+      setParsed({ vaa, vaaIndexes, tokenBridge, tokenBridgeIndexes });
     } catch (e) {
       console.error(e);
     }
   }, [vaaString]);
+  const [hoverIndex, setHoverIndex] = useState<string | null>(null);
+  const handleHover = useCallback((e: any) => {
+    if (e.type === "mouseenter") {
+      setHoverIndex(e.target.dataset.index);
+    } else {
+      setHoverIndex(null);
+    }
+  }, []);
+  useEffect(() => {
+    const isHex = /^(0[xX])?[A-Fa-f0-9]+$/.test(vaaString);
+    if (inputRef.current) {
+      const indexes =
+        hoverIndex &&
+        (hoverIndex.startsWith("payload-")
+          ? parsed?.tokenBridgeIndexes?.[hoverIndex.substring(8)]?.map(
+              (n) => n + (parsed?.vaaIndexes?.payload?.[0] || 0)
+            )
+          : parsed?.vaaIndexes?.[hoverIndex]);
+      if (isHex && indexes) {
+        inputRef.current.selectionStart = indexes[0] * 2;
+        inputRef.current.selectionEnd = indexes[1] * 2;
+      } else {
+        inputRef.current.selectionStart = inputRef.current.selectionEnd;
+        inputRef.current.blur();
+      }
+    }
+  }, [vaaString, hoverIndex, parsed]);
   return (
     <Grid container spacing={2}>
       <Grid xs={12} md={6} item>
@@ -121,6 +200,8 @@ export function DecoderComponent({
               value={vaaString}
               onChange={handleHexChange}
               disabled={!handleHexChange}
+              inputRef={inputRef}
+              sx={{ "& ::selection": { background: highlightColor } }}
             />
           </CardContent>
         </Card>
@@ -134,18 +215,27 @@ export function DecoderComponent({
                 <Typography variant="h6" gutterBottom>
                   Header
                 </Typography>
-                <pre style={{ overflowX: "auto" }}>
-                  {vaaToString(parsed.vaa)}
-                </pre>
+                <Box sx={preBoxStyle}>
+                  <pre style={{ pointerEvents: "none" }}>{"{"}</pre>
+                  {vaaToString(parsed.vaa, handleHover)}
+                  <pre style={{ pointerEvents: "none" }}>{"}"}</pre>
+                </Box>
                 <Typography variant="h6" mt={4} gutterBottom>
                   Payload
                 </Typography>
                 {parsed.tokenBridge ? (
-                  <pre style={{ overflowX: "auto" }}>
-                    {tokenTransferToString(parsed.tokenBridge)}
-                  </pre>
+                  <Box sx={preBoxStyle}>
+                    <pre style={{ pointerEvents: "none" }}>{"{"}</pre>
+                    {tokenTransferToString(parsed.tokenBridge, handleHover)}
+                    <pre style={{ pointerEvents: "none" }}>{"}"}</pre>
+                  </Box>
                 ) : (
-                  <code style={{ wordBreak: "break-all" }}>
+                  <code
+                    style={{ wordBreak: "break-all" }}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleHover}
+                    data-index="payload"
+                  >
                     {parsed.vaa.payload.toString("hex")}
                   </code>
                 )}
--- src/utils/vaaToIndexes.ts
+++ src/utils/vaaToIndexes.ts
@@ -1,9 +1,10 @@
 // Adapted from https://github.com/wormhole-foundation/wormhole/blob/main/sdk/js/src/vaa/wormhole.ts
 import { Buffer } from "buffer";
+import { TokenBridgePayload } from "../sdk/tokenBridge";
 import { SignedVaa } from "../sdk/wormhole";
 
 export type VaaIndexes = {
-  [key: string]: [number, number];
+  [key: string]: [number, number] | undefined;
 };
 
 export function vaaToIndexes(vaa: SignedVaa): VaaIndexes {
@@ -16,7 +17,7 @@ export function vaaToIndexes(vaa: SignedVaa): VaaIndexes {
   return {
     version: [0, 1],
     guardianSetIndex: [1, 5],
-    guardianSignatures: [sigStart, sigEnd],
+    guardianSignatures: [5, sigEnd],
     timestamp: [sigEnd, sigEnd + 4],
     nonce: [sigEnd + 4, sigEnd + 8],
     emitterChain: [sigEnd + 8, sigEnd + 10],
@@ -24,5 +25,26 @@ export function vaaToIndexes(vaa: SignedVaa): VaaIndexes {
     sequence: [sigEnd + 42, sigEnd + 50],
     consistencyLevel: [sigEnd + 50, sigEnd + 51],
     payload: [sigEnd + 51, signedVaa.length],
+  };
+}
+
+export function tokenTransferPayloadToIndexes(payload: Buffer): VaaIndexes {
+  const payloadType = payload.readUInt8(0);
+  if (
+    payloadType !== TokenBridgePayload.Transfer &&
+    payloadType !== TokenBridgePayload.TransferWithPayload
+  ) {
+    throw new Error("not token bridge transfer VAA");
+  }
+  return {
+    payloadType: [0, 1],
+    amount: [1, 33],
+    tokenAddress: [33, 65],
+    tokenChain: [65, 67],
+    toAddress: [67, 99],
+    toChain: [99, 101],
+    fee: payloadType === 1 ? [101, 133] : undefined,
+    fromAddress: payloadType === 3 ? [101, 133] : undefined,
+    tokenTransferPayload: [133, payload.length],
   };
 }
